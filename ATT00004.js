6ca749ba61978e9e02b799cdf7317fdd1bef3bf6
Tosin-88 <44926463+Tosin-88@users.noreply.github.com>
2018-11-11 11:35:16 +0000

    Second Version
    Changed creation of textNodes to use Alert() instead

--- ATT00004.js
+++ ATT00004.js
@@ -1,38 +1,55 @@
 function validate() {
     var email1 = document.getElementById("email");
-    var email2 = document.getElementById("emailRepeat");
-    var emailDiv1 = document.getElementById("emailFormElement");
-    var emailDiv2 = document.getElementById("repeatEmailFormElement");
-    var emailMessage = document.getElementById("errorMessage");
-    var emailMessage2 = document.getElementById("errorMessage2");
+    var email2 = document.getElementById("email2");
+    var pw = document.getElementById("password");
+    var pw2 = document.getElementById("password2");
+    var aT = document.getElementById("agreeTerms");
+    // var emailDiv = document.getElementById("emailDiv");
+    // var emailMessage = document.getElementsByClassName("errorMessage");
+    // var emailMessage2 = document.getElementsByClassName("errorMessage2");
 
-    if (emailMessage){
-        emailDiv1.removeChild(errorMessage)
-    }if (emailMessage2){
-        emailDiv1.removeChild(errorMessage2)
-    }
+    // if (emailMessage) {
+    //     emailDiv.removeChild(errorMessage);
+    // }if (emailMessage2) {
+    //     emailDiv.removeChild(errorMessage2);
+    // }
   
 
 
-    if(email1.value == "" && email2.value == "") {
-        var errorMessage = document.createElement("p");
-        errorMessage.setAttribute("id", "errorMessage");
-        var errorText = document.createTextNode("This field cannot be blank");
-        errorMessage.appendChild(errorText);
-        emailDiv1.appendChild(errorMessage);
+    if(email1.value == "" || email2.value == "") {
+        alert("The email fields cannot be empty");
         return false;
 
     }
 
     if(email1.value != email2.value ) {
-        var errorMessage = document.createElement("p");
-        errorMessage.setAttribute("id", "errorMessage2");
-        var errorText = document.createTextNode("This field must match");
-        errorMessage.appendChild(errorText);
-        emailDiv1.appendChild(errorMessage);
+        alert("The email fields do not match");
+        return false;
+ 
+    }
+
+    if(pw.value == "" || pw2.value == "") {
+        alert("The password field cannot be empty");
         return false;
 
     }
+
+    if(pw.value != pw2.value ) {
+        alert("The password fields do not match");
+        return false;
+ 
+    }
+    // if(aT.value == "" ) {
+    //     alert("You must agree to the terms and conditions");
+    //     return false;
+
+    // }
+
+    
+
+    
+
+    
 
 
     //no validate errors 
